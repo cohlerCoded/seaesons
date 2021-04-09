c55e9fad313a80511d7ca8908a9b232fff225b9c
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-09 11:28:56 -0700

    naive err handling

--- src/index.js
+++ src/index.js
@@ -12,16 +12,24 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errMsg: "" };
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({lat: position.coords.latitude})
+        this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errMsg: err.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errMsg}
+      </div>
+    );
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
