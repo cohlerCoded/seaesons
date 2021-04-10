e05506c32733d25cdf864d37e03cdfd0a9ffa8cf
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-10 01:30:49 -0700

    broke ternary logic out into a config object

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,20 +1,28 @@
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   } else if (month < 3 && month > 8) {
-    return lat < 0 ? "Summer" : "Winter";
+    return lat < 0 ? "summer" : "winter";
   }
+};
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burrr, it's chilly!",
+    iconName: "snowflake",
+  },
 };
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "Winter" ? "Burrr, it's chilly!" : "Let's hit the beach!";
-  const icon = season === "Winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
