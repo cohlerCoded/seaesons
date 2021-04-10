803efa86486e7d225ddbd6f1fad4ab5543ca216a
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-10 00:33:42 -0700

    logic to determine season

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,5 +1,12 @@
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else if (month < 3 && month > 8) {
+    return lat < 0 ? "Summer" : "Winter";
+  }
+};
 const SeasonDisplay = (props) => {
-  return <div>{props.lat}</div>;
+  return <div>{getSeason(props.lat, new Date().getMonth())}</div>;
 };
 
 export default SeasonDisplay;
