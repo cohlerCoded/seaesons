781b271a9b6ca3961606699cfa1e25ae7a09eb6a
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-10 01:58:03 -0700

    added styling

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -19,10 +20,10 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
